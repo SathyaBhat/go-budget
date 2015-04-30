b49dac9f165961e53766fd9d0ef4c4872f3be2ac
SathyaBhat <sathya@sathyasays.com>
2015-05-01 02:28:28 +0530

    update db stuff

--- go-budget.go
+++ go-budget.go
@@ -46,10 +46,56 @@ create table accounts (
     updated_by char(250),
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`
-	_, err = db.Exec(createAccounts, nil)
+	_, err = db.Exec(createAccounts, nil)	
+	if err == nil {
+		log.Debug("Accounts table created")
+	} else {
+		log.Fatal("Error %s while creating accounts table", err.Error())
+	}
+
+	createPayees := `
+create table payees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name string NOT NULL,
+    location string,
+    created_by char(250),
+    created_date TIMESTAMP not NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_by char(250),
+    updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`
+	_, err = db.Exec(createPayees, nil)
+	if err == nil {
+		log.Debug("Payees table created")
+	} else {
+		log.Fatal("Error %s while creating Payees table", err.Error())
+	}
+	createTransactions := `
+create table transactions (
+    id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    date timestamp NOT NULL,
+    payee_id integer NOT NULL,
+    category_id integer NOT NULL,
+    details string(1000) NOT NULL,
+    amount int NOT NULL,
+    cleared_flag integer,
+    created_by char(250),
+    created_date timestamp NOT NULL DEFAULT current_timestamp,
+    updated_by char(250),
+    updated_date timestamp DEFAULT current_timestamp
+    );`
+	_, err = db.Exec(createTransactions, nil)
+	if err == nil {
+		log.Debug("Transactions table created")
+	} else {
+		log.Fatal("Error %s while creating Transactions table", err.Error())
+	}
 	defer db.Close()
+
 	return err
 }
+
+
+
 
 func main() {
 	log = lumber.NewConsoleLogger(lumber.DEBUG)
