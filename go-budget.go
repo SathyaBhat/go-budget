075daa752e8e6552f491f9cdf1da4fc88d00fd7d
SathyaBhat <sathya@sathyasays.com>
2015-05-31 17:59:06 +0530

    Basic CSV import.

--- go-budget.go
+++ go-budget.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	_ "fmt"
 	"github.com/jcelliott/lumber"
 	_ "github.com/mattn/go-sqlite3"
 	_ "io/ioutil"
-	"flag"
+	"os"
 )
 
 var (
@@ -13,15 +15,44 @@ var (
 )
 
 const DBNAME string = "db/budget.db"
-var importPtr = flag.Bool("import", false, "Use -import to import csv");
-var importFileName = flag.String("file","","Filename of the csv to be imported")
+
+var importPtr = flag.Bool("import", false, "Use -import to import csv")
+var importFileName = flag.String("file", "", "Filename of the csv to be imported")
+
+func fetchCSV(csvfilename string) (bool, error) {
+	log.Debug("Import filename: %s",csvfilename )
+	csvfile, err := os.Open(csvfilename)
+    
+	if err != nil {
+		log.Error("Error %s occurred", err.Error())
+		return false, err
+	}
+
+	defer csvfile.Close()
+
+	reader := csv.NewReader(csvfile)
+	reader.Comma = ','
+	reader.LazyQuotes = true
+	rawCSVdata, err := reader.ReadAll()
+
+	if err != nil {
+        log.Error("Error %s occurred", err.Error())
+		return false, err
+	}
+
+	for _, each := range rawCSVdata {
+		log.Info("Account: %s", each[0])
+	}
+	return true, err
+}
 func main() {
+
+
 	log = lumber.NewConsoleLogger(lumber.DEBUG)
 	log.Prefix("gbd")
-	
 
-	flag.Parse();
-	
+	flag.Parse()
+
 	createdb, _ := DbExists(DBNAME)
 	if !createdb {
 		log.Info("DB not found, creating")
@@ -34,13 +65,15 @@ func main() {
 		log.Info("DB found, not creating")
 
 	}
-	
+
 	if *importPtr {
 		log.Debug("Importing CSV")
 		log.Info("Not yet implemented")
 		if *importFileName == "" {
-			log.Error("Running in import mode, but filename not passed");
+			log.Error("Running in import mode, but filename not passed")
+			panic("CSV file not found or not passed")
 		}
+		_, _ = fetchCSV(*importFileName)
 	}
 
 }
